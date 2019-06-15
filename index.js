0cd4e94a77b8b6bbd01b9de393adfe3b12edf339
Allison Howard <alliemhow@gmail.com>
2019-06-15 17:23:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function writeCards () {
+function writeCards ( namesArray, event ) {
   let thankYouCards = []
   for ( let i = 0; i < namesArray.length; i++ ) {
     thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
