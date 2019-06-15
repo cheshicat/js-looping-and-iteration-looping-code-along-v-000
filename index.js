9df1a82a1fc9343579c3b52ca241e12e8457903c
Allison Howard <alliemhow@gmail.com>
2019-06-15 17:21:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function writeCards () {
   let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    
+  }
 }
 
 
-  let thankYouCards = []
-   for ( let i = 0; i < namesArray.length; i++ ) {
+
      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
    }
    return thankYouCards
